63e649d30dc90b0e88137d5fc663b4482c5d63ec
jmj <jmj@kookmin.ac.kr>
2023-06-05 23:52:32 +0900

    3.vote_comment applied

--- ADproject/pybo/models.py
+++ ADproject/pybo/models.py
@@ -32,10 +32,11 @@ class Answer(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comment')
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
     modify_count = models.IntegerField() # 댓글 수정 횟수 추가
+    voter = models.ManyToManyField(User, related_name='voter_comment')
